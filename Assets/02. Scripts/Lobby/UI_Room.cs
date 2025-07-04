99ab7e260260ae303cc34d2595b5b205202c9db3
Suhyeon Park <suhyeon12341@gmail.com>
2025-07-04 18:19:37 +0900

    Feat 로비씬 방 목록 레이아웃 구현

---
+++ Assets/02. Scripts/Lobby/UI_Room.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+// 뿡
+public class UI_Room : MonoBehaviour
+{
+    public TextMeshProUGUI RoomTitleTextUI;
+    public TextMeshProUGUI MasterNicknameTextUI;
+    public TextMeshProUGUI RoomPlayerCountTextUI;
+
+    public void OnClickCreateRoomButton()
+    {
+        
+    }
+    
+}
